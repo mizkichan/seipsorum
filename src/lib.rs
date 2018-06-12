f8deef322de38391d6c4d309ed640d2af544db54
Mizki SUZUMORI <suzumorimizuki@gmail.com>
2018-06-12 19:30:43 +0900

    クレートのドキュメントを更新

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-//! # seipsorum
-//!
 //! seipsorum: from Latin _seipsorum_, the genitive neuter plural of _seipse_.
+//!
+//! この文書において断りなくページ番号や章番号を示す場合は，『日本語文法体系新論』(清瀬義三郎則府著，ひつじ書房，2013年) におけるものを指すものとする．
 
 #![feature(box_syntax)]
 #![deny(missing_docs)]
